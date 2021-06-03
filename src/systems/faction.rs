0882504526df61088ce9cd71fd9312afee1ca69d
Horup <sah@sah.dk>
2021-06-03 05:42:45 +0200

    faction coloring is now done for children as well

--- src/systems/faction.rs
+++ src/systems/faction.rs
@@ -1,11 +1,26 @@
+use std::process::Child;
+
 use bevy::prelude::*;
 
 use crate::Faction;
 
-pub fn faction_system(mut query:Query<(&Faction, &mut TextureAtlasSprite)>) {
-    query.for_each_mut(|(faction, mut sprite)| {
-        if sprite.color != faction.primary_color() {
-            sprite.color = faction.primary_color();
+pub fn faction_system(mut query:Query<(Entity, &Faction)>, children:Query<&Children>, mut sprites:Query<&mut TextureAtlasSprite>) {
+    query.for_each_mut(|(e, faction)| {
+        if let Ok(mut sprite) = sprites.get_component_mut::<TextureAtlasSprite>(e) {
+            set_color(faction, &mut sprite);
+        }
+        if let Ok(children) = children.get_component::<Children>(e) {
+            for child in children.iter() {
+                if let Ok(mut sprite) = sprites.get_component_mut::<TextureAtlasSprite>(*child) {
+                    set_color(faction, &mut sprite);
+                }
+            }
         }
     });
+}
+
+fn set_color(faction:&Faction, sprite:&mut Mut<TextureAtlasSprite>) {
+    if sprite.color != faction.primary_color() {
+        sprite.color = faction.primary_color();
+    }
 }
