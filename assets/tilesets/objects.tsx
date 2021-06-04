e71c4ea0730abbc41cda97343282cee66861ae08
Horup <sah@sah.dk>
2021-06-04 08:47:12 +0200

    started working om map support, moved graphics

---
+++ assets/tilesets/objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="objects" tilewidth="8" tileheight="8" tilecount="4" columns="2">
+ <image source="../imgs/objects.png" width="16" height="16"/>
+</tileset>
