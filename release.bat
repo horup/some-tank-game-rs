2681e9803c328ae85fc6d379f239157eaafdb9be
Søren Alsbjerg Hørup <sah@sah.dk>
2021-06-25 08:35:16 +0200

    updated

--- release.bat
+++ release.bat
@@ -2,5 +2,5 @@ rmdir dist /s /q 2>nul
 rmdir dist /s /q 2>nul
 mkdir dist
 xcopy target\release\*.exe dist\
-copy config_default.ini dist\config.ini
+copy config_release.ini dist\config.ini
 xcopy assets dist\assets /E /H /C /I
