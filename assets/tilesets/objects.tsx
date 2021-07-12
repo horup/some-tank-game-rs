44dd6e00b9f6b7559eba6eb543d5aa09fd3d1814
horup <horup@deepstacker.com>
2021-07-12 18:15:23 +0000

    Deploying to gh-pages from @ horup/some-tank-game-rs@3af34da552cfcb9e27e0693e565aa655bccda9f2 🚀

---
+++ assets/tilesets/objects.tsx
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="objects" tilewidth="8" tileheight="8" tilecount="4" columns="2">
+ <image source="../imgs/objects.png" width="16" height="16"/>
+ <tile id="0" type="player"/>
+ <tile id="1" type="bot"/>
+</tileset>
---
+++ assets/tilesets/tiles.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="tiles" tilewidth="8" tileheight="8" tilecount="16" columns="4">
+ <image source="../imgs/tiles.png" width="32" height="32"/>
+ <tile id="1">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
