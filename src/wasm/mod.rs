d0684cd8b476440de10938e56f9198fbd63f45ae
Horup <sah@sah.dk>
2021-07-15 22:16:43 +0200

    added explicit resource to hold old size

--- src/wasm/mod.rs
+++ src/wasm/mod.rs
@@ -8,27 +8,37 @@ extern "C" {
     fn resize_canvas(width:f32, height:f32);
 }
 
-fn resizer(mut windows:ResMut<Windows>, mut window_resized_events: EventWriter<WindowResized>, wd:Res<WindowDescriptor>) {
+struct LastSize {
+    pub width:f32,
+    pub height:f32
+}
+
+fn resizer(mut windows:ResMut<Windows>, mut window_resized_events: EventWriter<WindowResized>, wd:Res<WindowDescriptor>, mut last_size:ResMut<LastSize>) {
     let window = web_sys::window().expect("no global `window` exists");
     let mut width:f32 = window.inner_width().unwrap().as_f64().unwrap() as f32;
     let mut height:f32 = window.inner_height().unwrap().as_f64().unwrap() as f32;
 
     if let Some(window) = windows.get_primary_mut() {
-        if window.width() != width || window.height() != height {
-            width = if width < wd.resize_constraints.min_width { wd.resize_constraints.min_width } else { width };
-            width = if width > wd.resize_constraints.max_width { wd.resize_constraints.max_width } else { width };
-            height = if height < wd.resize_constraints.min_height { wd.resize_constraints.min_height } else { height };
-            height = if height > wd.resize_constraints.max_height { wd.resize_constraints.max_height } else { height };
+            if width != last_size.width || height != last_size.height {
+                *last_size = LastSize {
+                    width, height
+                };
 
-            window.update_actual_size_from_backend(width as u32, height as u32);
-            window_resized_events.send(WindowResized {
-                id:window.id(),
-                height:height,
-                width:width
-            });
-            resize_canvas(width, height);
-            info!("Resizing to {:?},{:?}", width, height);
-        }
+                width = if width < wd.resize_constraints.min_width { wd.resize_constraints.min_width } else { width };
+                width = if width > wd.resize_constraints.max_width { wd.resize_constraints.max_width } else { width };
+                height = if height < wd.resize_constraints.min_height { wd.resize_constraints.min_height } else { height };
+                height = if height > wd.resize_constraints.max_height { wd.resize_constraints.max_height } else { height };
+
+                window.update_actual_size_from_backend(width as u32, height as u32);
+                window_resized_events.send(WindowResized {
+                    id:window.id(),
+                    height:height,
+                    width:width
+                });
+                resize_canvas(width, height);
+                info!("Resizing to {:?},{:?}", width, height);
+            }
+
     }
 }
 
@@ -38,6 +48,7 @@ pub struct WASMPlugin;
 impl Plugin for WASMPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app.add_plugin(bevy_webgl2::WebGL2Plugin);
+        app.insert_resource(LastSize { width:0.0, height:0.0});
         app.add_system(resizer.system());
     }
 }
